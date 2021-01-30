63f40307155341f83751f462a580fd4bfb760c51
mahbub095 <mahbub.095@gmail.com>
2021-01-30 12:41:20 +0600

    Migrate Database

--- database/migrations/2020_09_04_195355_create_module_role_table.php
+++ database/migrations/2020_09_04_195355_create_module_role_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateModuleRoleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('module_role', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('module_id');
+            $table->foreign('module_id')->references('id')->on('modules');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('module_role');
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,8 @@
     <title>JustLog - Login</title>
     <!-- External CSS -->
     <link type="text/css" rel="stylesheet" href="{{('backend/login/assets/css/bootstrap.min.css')}}">
-    <link type="text/css" rel="stylesheet" href="{{('backend/login/assets/fonts/font-awesome/css/font-awesome.min.css')}}">
+    <link type="text/css" rel="stylesheet"
+          href="{{('backend/login/assets/fonts/font-awesome/css/font-awesome.min.css')}}">
 
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="32x32" href="{{('backend/login/images/fev.png')}}">
@@ -21,7 +22,9 @@
 <body>
 
 <!-- Loader -->
-<div class="loader"><div class="loader_div"></div></div>
+<div class="loader">
+    <div class="loader_div"></div>
+</div>
 
 <!-- Login page -->
 <div class="login_wrapper">
@@ -29,7 +32,8 @@
 
         <div class="col-md-6 mobile-hidden">
             <div class="login_left">
-                <div class="login_left_img"><img src="{{('backend/login/images/login-bg.jpg')}}" alt="login background"></div>
+                <div class="login_left_img"><img src="{{('backend/login/images/login-bg.jpg')}}" alt="login background">
+                </div>
             </div>
         </div>
         <div class="col-md-6 bg-white">
@@ -39,28 +43,47 @@
                 </a>
                 <div class="login_form">
                     <div class="login_form_inner">
-                        <form action="" method="GET">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
                             <div class="form-group">
-                                <input type="email" name="email" class="input-text" placeholder="Email Address">
+                                <input type="email" name="email"
+                                       class="input-text  @error('email') is-invalid @enderror"
+                                       placeholder="Email Address" value="{{ old('email') }}" required
+                                       autocomplete="email" autofocus>
                                 <i class="fa fa-envelope"></i>
-                                <span class="focus-border"></span>
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                @enderror
                             </div>
                             <div class="form-group">
-                                <input type="password" name="Password" class="input-text" placeholder="Password">
-                                <i class="fa fa-lock"></i>
-                                <span class="focus-border"></span>
+                                <input type="password" name="password"
+                                       class="input-text @error('password') is-invalid @enderror" placeholder="Password"
+                                       required autocomplete="current-password">
+                                <i class="fa fa-lock  "></i>
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="checkbox clearfix">
                                 <div class="form-check checkbox-theme">
-                                    <input class="form-check-input" type="checkbox" value="" id="rememberMe">
+                                    <input class="form-check-input" type="checkbox" name="remember" value=""
+                                           id="rememberMe" {{ old('remember') ? 'checked' : '' }}>
                                     <label class="form-check-label" for="rememberMe">
                                         Remember me
                                     </label>
                                 </div>
-                                <a href="forgot-password.html">Forgot Password</a>
+                                @if (Route::has('password.request'))
+                                    <a href="{{ route('password.request') }}">Forgot Password</a>
+                                @endif
                             </div>
                             <div class="form-group">
-                                <button type="submit" class="btn-md btn-theme btn-block">Login</button>
+                                <button type="submit" class="btn-md btn-theme btn-block"><i
+                                        class="fas fa-sign-in-alt"></i> Login
+                                </button>
                             </div>
                         </form>
                     </div>
@@ -74,7 +97,8 @@
 
 
 <!-- External JS libraries -->
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
 <!-- Custom JS Script -->
 <script type="text/javascript">
